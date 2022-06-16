66ff4b2dda24fbc610748751d64c6df9abe27309
Mostainahmed <mostainahmed@gmail.com>
2022-06-16 21:24:51 +0200

    Added Laravel propject files and Uuid Trait generator class file

---
+++ app/Models/CarType.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Uuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CarType extends Model
+{
+    use HasFactory, Uuids;
+    protected $fillable = [
+        ''
+    ];
+}
---
+++ app/Traits/Uuids.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Traits;
+
+use Illuminate\Support\Str;
+
+trait Uuids
+{
+    protected static function bootUsesUuid()
+    {
+        static::creating(function ($model) {
+            if (! $model->getKey()) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    public function getKeyType()
+    {
+        return 'string';
+    }
+}
